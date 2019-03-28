b98f11b92527d0108055da175f7ad18f7d3bd32b
reikoyanagi <pxfpd899>
2019-03-28 20:17:34 +0900

    ログイン中のユーザー名の非表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(5)
     respond_to do |format|
      format.html
      format.json
